7e1f0863f65df600dd9699e494e9d481fcb99eb9
MMBIKAI <mohanad_bikai@hotmail.com>
2025-04-14 10:23:42 +0200

    creation of the query to get country by code

--- src/resolvers/CountryResolver.ts
+++ src/resolvers/CountryResolver.ts
@@ -1,10 +1,18 @@
 import { Country } from "../entities/Country";
 import { Arg, Mutation, Query } from "type-graphql";
+import { FindManyOptions, Like } from "typeorm";
 
 export class CountryResolver {
     @Query(() => [Country])
     async GetAllCountries() {
         return await Country.find();
+    }
+    @Query(() => Country, { nullable: true })
+    async GetCountryByCode(@Arg("code") code: string) {
+        const countrySearch: FindManyOptions<Country> = {
+            where: { code: Like(`%${code}%`) },
+        };
+        return await Country.findOne(countrySearch);
     }
 
     @Mutation(() => Country)
